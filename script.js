df96b47650a63c9ed515e50cb1fa173f81224a09
Jyoti Prakash <jpchiranjiv22@gmail.com>
2024-03-21 12:24:59 +0530

    done

--- script.js
+++ script.js
@@ -8,20 +8,38 @@ function addTask(){
     else{
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
+        const id = new Date().getTime();
+        span.addEventListener("click", (e)=> onItemDelete(e,id));
         li.appendChild(span);
+        li.setAttribute("id",id);
+        li.addEventListener("click",(e)=> onListItemClick(e,id));
+        listContainer.appendChild(li);
     }
     inputBox.value = "";
 }
 
-listContainer.addEventListener("click", function(e){
-    if(e.target.tagname === "LI"){
-        console.log(e)
-    e.target.classList.toggle("checked");
-    }
-    else if(e.target.tagname === "SPAN"){
-        e.target.parentElement.remove();
-    }
-},false);
+const onListItemClick = (e,id)=> {
+    e.preventDefault();
+    const item = document.getElementById(id);
+    item.classList.toggle("checked");
+}
+
+const onItemDelete = (e,id)=> {
+    e.preventDefault();
+    const item = document.getElementById(id);
+    item.remove();
+}
+
+// listContainer.addEventListener('click', function(e){
+//     console.log("hello");
+//     if(e.target.tagname === "LI"){
+//         // console.log(e)
+//     e.target.classList.toggle("checked");
+//     }
+//     else if(e.target.tagname === "SPAN")
+//     {
+//         e.target.parentElement.remove();
+//     }
+// },false);
